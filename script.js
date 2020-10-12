630ece5f5e1b2f622894a6b15b01501beb16a59d
zb45659 <benckendorfzac@johndeere.com>
2020-10-12 09:04:29 -0500

    first AI step complete

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 // let buttons = document.querySelectorAll(".buttons")
-let green = document.getElementById("green")[0];
-let red = document.getElementById("red")[0];
-let yellow = document.getElementById("yellow")[0];
-let blue = document.getElementById("blue")[0];
-let startButton = document.querySelector(".start");
+// let green = document.getElementById("green")[0];
+// let red = document.getElementById("red")[0];
+// let yellow = document.getElementById("yellow")[0];
+// let blue = document.getElementById("blue")[0];
+const startButton = document.querySelector(".start");
 
 let sequence = [];
 let userSequence = [];
 let level = 0;
 
-
-
-function startGame() {
-    console.log("game started");
-}
-
-
 function activateButton(color) {
     const button = document.querySelector(`[data-tile='${color}']`);
-
+    
     button.classList.add('flash');
-
+    
     setTimeout(() => {
         button.classList.remove('flash');
     }, 500);    
@@ -30,13 +23,9 @@ function playLevel(nextSequence) {
     nextSequence.forEach((color, index) => {
         setTimeout(() => {
             activateButton(color);
-        }, (index = 1) * 500);
+        }, (index + 1) * 500);
     });
 }
-
-
-
-
 
 function randomStep() {
     const buttons = ['green','red','yellow','blue'];
@@ -49,7 +38,13 @@ function nextLevel() {
     //array used to store current sequence + new Sequence
     const nextSequence = [];
     nextSequence.push(randomStep());
+    playLevel(nextSequence);
 }
+function startGame() {
+    nextLevel();
+}
+
+
 startButton.addEventListener('click',startGame);
 
 // activateButton(random);
