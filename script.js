b12915d23ed3aaa51219cb9f212163e0a960a951
zb45659 <benckendorfzac@johndeere.com>
2020-10-12 13:47:36 -0500

    user tunrs and reset game added

--- script.js
+++ script.js
@@ -2,32 +2,45 @@
 // let red = document.getElementByClassName("red");
 // let yellow = document.getElementByClassName("yellow");
 // let blue = document.getElementByClassName("blue");
-const startButton = document.querySelector(".start");
-
-let sequence = [];
+let startButton = document.querySelector(".start");
 let level = 0;
 let userSequence = [];
-const nextSequence = [];
+let nextSequence = [];
 let buttons = document.querySelectorAll(".buttons")
+
+function resetGame() {
+    alert("You have lost, try again!");
+    userSequence = [];
+    nextSequence = [];
+    level = 0;
+}
+
+
 
 function addSequence(btn) {
     userSequence.push(btn.getAttribute('data-button'));
+    btn.classList.add('flash');
+    
+    setTimeout(() => {
+        btn.classList.remove('flash');
+    }, 250); 
     console.log(userSequence);
 
-
-    // console.log(btn.getAttribute('data-button'));
-};
-
-
+    if (userSequence.length == nextSequence.length) { 
+    if (JSON.stringify(userSequence) == JSON.stringify(nextSequence)) {
+        console.log("Good job, keep going")
+        nextLevel()
+    } else {resetGame()};
+    } else {console.log("more buttons needed")};
+}
 
 function buttonClicked() {
     for (let i = 0; i < buttons.length; i++){
         buttons[i].onclick = function() {
             addSequence(this);
-        }
     }
 }
-
+}
 
 function activateButton(color) {
     const button = document.querySelector(`[data-button='${color}']`);
@@ -36,14 +49,14 @@ function activateButton(color) {
     
     setTimeout(() => {
         button.classList.remove('flash');
-    }, 500);    
+    }, 1000);    
 }
 
 function playLevel(newSequence) {
     newSequence.forEach((color, index) => {
         setTimeout(() => {
             activateButton(color);
-        }, (index + 1) * 500);
+        }, (index + 1) * 1500);
     });
 }
 
@@ -54,12 +67,16 @@ function randomStep() {
 }
 
 function nextLevel() {
+    // if (JSON.stringify(userSequence) == JSON.stringify(nextSequence)) {
     level += 1;
+    userSequence = [];
     //array used to store current sequence + new Sequence
     nextSequence.push(randomStep());
     playLevel(nextSequence);
     console.log(nextSequence)
+    // } else {resetGame()};
 }
+
 function startGame() {
     nextLevel();
 }
@@ -68,9 +85,3 @@ startButton.addEventListener('click',startGame);
 
 // activateButton(random);
 // console.log(green);
-
-
-
-
-
-
