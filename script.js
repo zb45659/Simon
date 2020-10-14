5c6bb937f29c3859db7e8b8ec164567d7fd0fe8f
zb45659 <benckendorfzac@johndeere.com>
2020-10-14 09:34:46 -0500

    JS cleaned

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
 let nameButton = document.querySelector("#name-button");
-let nameText = document.querySelector("#name-txt");
-let highscoreList = document.querySelector(".high-score"); 
-let container = document.querySelector("#container");
+let buttons = document.querySelectorAll(".buttons");
 let startButton = document.querySelector(".startButton");
 let resetButton = document.querySelector(".reset");
-let buttons = document.querySelectorAll(".buttons");
+
+let container = document.querySelector("#container");
 let scoreBoard = document.querySelector(".scoreBoard");
-let level = 0;
-let userSequence = [];
-let nextSequence = [];
+let nameText = document.querySelector("#name-txt");
+let highscoreList = document.querySelector(".high-score"); 
+
 let setHighScore = localStorage.setItem('highscore',15);
 let highscore = localStorage.getItem('highscore');
+
+let userSequence = [];
+let nextSequence = [];
 let highScores = [];
+let level = 0;
 
 
-nameButton.addEventListener('click', function storeScore(e) {
-    e.preventDefault();
-    if(nameText.value !== "") {
-        let userValue = {name: "", score: ""};
-        userValue.name = "Zac";
-        userValue.score = highscore;
-        highScores.push({userValue});
-        highscoreList.innerHTML = `Name: ${userValue.name}\nScore: ${userValue.score}`;
-        nameText.value = "";
-} else (alert("You need to enter your name!!"));
-});
 
 
 function resetGame() {
-    console.log(level);
-    console.log(parseInt(highscore));
     if(highscore !== null){
         if (level > parseInt(highscore)) {
         localStorage.setItem('highscore',level);
-        console.log(highscore);
         } else {
             localStorage.setItem('highscore',level);
-        }
-    }
+    }}
     userSequence = [];
     nextSequence = [];
     level = 0;
@@ -45,12 +33,9 @@ function resetGame() {
     container.setAttribute("class","cantClick");
 }
 
-
-
 function addSequence(btn) {
     userSequence.push(btn.getAttribute('data-button'));
     btn.classList.add('flash');
-    // console.log(btn.dataset.button);
 
     const soundClick = document.querySelector(`[data-sound='${btn.dataset.button}']`);
     soundClick.play();
@@ -58,32 +43,25 @@ function addSequence(btn) {
     setTimeout(() => {
         btn.classList.remove('flash');
     }, 250); 
-    // console.log(userSequence);
     
     if( userSequence.length == nextSequence.length) {
     if (JSON.stringify(userSequence) == JSON.stringify(nextSequence)) {
-            console.log("Good job, keep going")
             nextLevel()
    } else {
             resetGame();
             alert("You have lost, try again!");
-        }
-    }
-}
-
+}}}
 
 function buttonClicked() {
     for (let i = 0; i < buttons.length; i++){
         buttons[i].onclick = function() {
             addSequence(this);
-        }
-    }
-}
+}}}
 
 function activateButton(color) {
     const button = document.querySelector(`[data-button='${color}']`);
     const sound = document.querySelector(`[data-sound='${color}']`);
-    // console.log(color);
+
     button.classList.add('flash');
     sound.play();
 
@@ -100,10 +78,7 @@ function playLevel(newSequence) {
         setTimeout(() => {
             container.classList.remove("cantClick");
             }, (index +1) * 2500);
-    });
-    
-        
-}
+})}
 
 function randomStep() {
     const buttons = ['green','red','yellow','blue'];
@@ -112,52 +87,31 @@ function randomStep() {
 }
 
 function nextLevel() {
-    // if (JSON.stringify(userSequence) == JSON.stringify(nextSequence)) {
-        level += 1;
-        scoreBoard.textContent = level;
-        container.setAttribute("class","cantClick");
-        userSequence = [];
-        //array used to store current sequence + new Sequence
-        nextSequence.push(randomStep());
-        playLevel(nextSequence);
-        // console.log(nextSequence)
-        // } else {resetGame()};
-    }
+    level += 1;
+    scoreBoard.textContent = level;
+    container.setAttribute("class","cantClick");
+    userSequence = [];
+    nextSequence.push(randomStep());
+    playLevel(nextSequence);
+}
 
 function startGame() {
     if (level == 0) {
     nextLevel();
-} else {console.log("Game already going!");
-}
-}
+} else {alert("Game already going!");
+}}
 
 startButton.addEventListener('click',startGame);
 resetButton.addEventListener('click', resetGame);
-// activateButton(random);
-// console.log(green);
 
-
-// const Errors = document.getElementById('error');
-
-// function get_scores (callback) {
-//   // High Score Data
-//   let file = "scores.json";
-
-//   // Fetch High Score Data
-//   fetch(file, {cache: 'no-cache'})
-//     .then(function(response) {
-//         //  If the response isn't OK
-//         if (response.status !== 200) {
-//           Errors.innerHTML = response.status;
-//         }
-//         // If the response is OK
-//         response.json().then(function(data) {
-//           let scores = JSON.stringify(data);
-//           console.log(scores);
-//           callback (scores);
-//         });
-//       })
-//     // If there is an error
-//     .catch(function(err) {
-//       Errors.innerHTML = err;
-//     });
+nameButton.addEventListener('click', function storeScore(e) {
+    e.preventDefault();
+    if(nameText.value !== "") {
+        let userValue = {name: "", score: ""};
+        userValue.name = "Zac";
+        userValue.score = highscore;
+        highScores.push({userValue});
+        highscoreList.innerHTML = `Name: ${userValue.name} Score: ${userValue.score}`;
+        nameText.value = "";
+} else (alert("You need to enter your name!!"));
+})
