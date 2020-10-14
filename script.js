9c1a05a35efdcc18789e27ef956d1e80bedc6bb2
zb45659 <benckendorfzac@johndeere.com>
2020-10-14 07:28:27 -0500

    css created

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// let green = document.getElementsByClassName("green");
-// let red = document.getElementByClassName("red");
-// let yellow = document.getElementByClassName("yellow");
-// let blue = document.getElementByClassName("blue");
+let nameButton = document.querySelector("#name-button");
+let nameText = document.querySelector("#name-txt");
+let highscoreList = document.querySelector(".high-score"); 
 let container = document.querySelector("#container");
 let startButton = document.querySelector(".startButton");
 let resetButton = document.querySelector(".reset");
@@ -10,8 +9,35 @@ let scoreBoard = document.querySelector(".scoreBoard");
 let level = 0;
 let userSequence = [];
 let nextSequence = [];
+let setHighScore = localStorage.setItem('highscore',15);
+let highscore = localStorage.getItem('highscore');
+let highScores = [];
+
+
+nameButton.addEventListener('click', function storeScore(e) {
+    e.preventDefault();
+    if(nameText.value !== "") {
+        let userValue = {name: "", score: ""};
+        userValue.name = "Zac";
+        userValue.score = highscore;
+        highScores.push({userValue});
+        highscoreList.innerHTML = `Name: ${userValue.name}\nScore: ${userValue.score}`;
+        nameText.value = "";
+} else (alert("You need to enter your name!!"));
+});
+
 
 function resetGame() {
+    console.log(level);
+    console.log(parseInt(highscore));
+    if(highscore !== null){
+        if (level > parseInt(highscore)) {
+        localStorage.setItem('highscore',level);
+        console.log(highscore);
+        } else {
+            localStorage.setItem('highscore',level);
+        }
+    }
     userSequence = [];
     nextSequence = [];
     level = 0;
@@ -34,16 +60,17 @@ function addSequence(btn) {
     }, 250); 
     // console.log(userSequence);
     
-    if (userSequence.length == nextSequence.length) { 
-        if (JSON.stringify(userSequence) == JSON.stringify(nextSequence)) {
+    if( userSequence.length == nextSequence.length) {
+    if (JSON.stringify(userSequence) == JSON.stringify(nextSequence)) {
             console.log("Good job, keep going")
             nextLevel()
-        } else {
+   } else {
             resetGame();
             alert("You have lost, try again!");
         }
-    } else {console.log("more buttons needed")};
+    }
 }
+
 
 function buttonClicked() {
     for (let i = 0; i < buttons.length; i++){
@@ -108,3 +135,29 @@ startButton.addEventListener('click',startGame);
 resetButton.addEventListener('click', resetGame);
 // activateButton(random);
 // console.log(green);
+
+
+// const Errors = document.getElementById('error');
+
+// function get_scores (callback) {
+//   // High Score Data
+//   let file = "scores.json";
+
+//   // Fetch High Score Data
+//   fetch(file, {cache: 'no-cache'})
+//     .then(function(response) {
+//         //  If the response isn't OK
+//         if (response.status !== 200) {
+//           Errors.innerHTML = response.status;
+//         }
+//         // If the response is OK
+//         response.json().then(function(data) {
+//           let scores = JSON.stringify(data);
+//           console.log(scores);
+//           callback (scores);
+//         });
+//       })
+//     // If there is an error
+//     .catch(function(err) {
+//       Errors.innerHTML = err;
+//     });
